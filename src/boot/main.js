953ec094784984ad7ba43ff6bee4e6fa2655f2b4
cloudWoR <756590854@qq.com>
2021-01-23 14:33:38 +0800

    推送 electron 分支

--- src/boot/main.js
+++ src/boot/main.js
@@ -7,9 +7,13 @@ import '../components/Markdown/Markdown'
 import '../quasar.manage.config'
 import '../axios/fetchData'
 import { handleBaiduStatistics } from 'src/utils/clone-utils'
+import { colors } from 'quasar'
 
 // "async" is optional;
 // more info on params: https://quasar.dev/quasar-cli/boot-files
 export default async ({ app, router, Vue, publicPath }) => {
+  // 解构只保留需要的东西
+  const { setBrand } = colors
+  setBrand('mycolor', '#f33')
   handleBaiduStatistics()
 }
